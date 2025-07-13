de0e1591e92c6356aa6115236bae56be7852778a
314159otr <piotrbpersonal@gmail.com>
2025-07-13 13:44:21 +0200

    L5: Messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -11,8 +12,13 @@ def main():
     if len(sys.argv)==1:
         print("No prompt was passed")
         sys.exit(1)
-    prompt = sys.argv[1]
-    response = client.models.generate_content(model="gemini-2.0-flash-001",contents=prompt)
+
+   
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
