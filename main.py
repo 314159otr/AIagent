ac2c56c3ff9282e639193f2a1d92b8d3b1d1676e
314159otr <piotrbpersonal@gmail.com>
2025-07-13 13:52:40 +0200

    L6: Verbose

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ def main():
         print("No prompt was passed")
         sys.exit(1)
 
-   
     user_prompt = sys.argv[1]
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
     response = client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
