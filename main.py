33cb746b8ddc268af529858bacaef318e1f500d0
314159otr <piotrbpersonal@gmail.com>
2025-07-13 13:38:40 +0200

    L4: Input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,7 +8,10 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    if len(sys.argv)==1:
+        print("No prompt was passed")
+        sys.exit(1)
+    prompt = sys.argv[1]
     response = client.models.generate_content(model="gemini-2.0-flash-001",contents=prompt)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
